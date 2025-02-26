ea93fa462b3057c44f82fcfcdd34927bf0b059c9
ZhuangYumin <zhuangyumin@sjtu.edu.cn>
2025-02-26 09:25:31 +0800

    feat: allow o1 streaming

--- src/renderer/src/providers/OpenAIProvider.ts
+++ src/renderer/src/providers/OpenAIProvider.ts
@@ -197,12 +197,7 @@ export default class OpenAIProvider extends BaseProvider {
       userMessages.push(await this.getMessageParam(message, model))
     }
 
-    const isOpenAIo1 = this.isOpenAIo1(model)
-
     const isSupportStreamOutput = () => {
-      if (isOpenAIo1) {
-        return false
-      }
       return streamOutput
     }
 
@@ -329,13 +324,8 @@ export default class OpenAIProvider extends BaseProvider {
         ]
       : [{ role: 'user', content: assistant.prompt }]
 
-    const isOpenAIo1 = this.isOpenAIo1(model)
-
     const isSupportedStreamOutput = () => {
       if (!onResponse) {
-        return false
-      }
-      if (isOpenAIo1) {
         return false
       }
       return true
